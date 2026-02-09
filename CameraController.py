48b9c7fe50e8fbbf0395fec3a7b012765d4e20fd
Iván García <ivigaga2004@gmail.com>
2026-02-09 17:37:10 +0100

    Refactorización
    Desacoplamiento de los umbrales a una estructura de datos

--- CameraController.py
+++ CameraController.py
@@ -7,21 +7,14 @@ import time
 
 # --- CONFIGURACIÓN ---
 MODEL_PATH = 'face_landmarker.task' 
-UMBRAL_BOCA = 0.4
-UMBRAL_CEJAS = 0.7
-UMBRAL_MORRITOS = 0.8
-UMBRAL_SONRISA = 0.5
-
-
-buttons={"guiño":vg.XUSB_BUTTON.XUSB_GAMEPAD_A, "boca":vg.XUSB_BUTTON.XUSB_GAMEPAD_B, "cejas":vg.XUSB_BUTTON.XUSB_GAMEPAD_START, "morritos":vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK,"sonrisa":vg.XUSB_BUTTON.XUSB_GAMEPAD_B}
-
+input_structure={"jawOpen":{"input":vg.XUSB_BUTTON.XUSB_GAMEPAD_B,"threshold":0.4},"eyeBrowsUp":{"input":vg.XUSB_BUTTON.XUSB_GAMEPAD_START,"threshold":0.4},"mouthPucker":{"input":vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK,"threshold":0.8},"smile":{"input":vg.XUSB_BUTTON.XUSB_GAMEPAD_B,"threshold":0.5},"eyeBlinkRight":{"input":vg.XUSB_BUTTON.XUSB_GAMEPAD_A,"threshold":0.5}}
 
 # Variables globales
-RESULTADO_ACTUAL = None
+CURRENT_RESULT = None
 
 def resultado_callback(result, output_image, timestamp_ms):
-    global RESULTADO_ACTUAL
-    RESULTADO_ACTUAL = result
+    global CURRENT_RESULT
+    CURRENT_RESULT = result
 
 base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
 options = vision.FaceLandmarkerOptions(
@@ -42,8 +35,7 @@ while cap.isOpened():
     ret, frame = cap.read()
     if not ret: break
 
-    # 1. PASO CRUCIAL: Invertir AL PRINCIPIO
-    # Ahora todo lo que hagamos (detectar y dibujar) será sobre la imagen espejo
+
     
 
     # Convertir para MediaPipe
@@ -53,8 +45,8 @@ while cap.isOpened():
 
     detector.detect_async(mp_image, frame_timestamp_ms)
 
-    if RESULTADO_ACTUAL and RESULTADO_ACTUAL.face_blendshapes:
-        gestos = RESULTADO_ACTUAL.face_blendshapes[0]
+    if CURRENT_RESULT and CURRENT_RESULT.face_blendshapes:
+        gestures = CURRENT_RESULT.face_blendshapes[0]
         
         # --- 2. EL TRUCO DEL ESPEJO (SWAP LOGIC) ---
         # Como la imagen está invertida, MediaPipe se lía con izquierda/derecha.
@@ -63,87 +55,78 @@ while cap.isOpened():
         # Ceja: Si queremos detectar tu ceja REAL derecha, 
         # leemos lo que MediaPipe llama "OuterUpLeft" (porque está viendo un espejo)
         # Nota: Para las cejas usábamos el MAX de las dos, así que da igual.
-        ceja_L = next((x.score for x in gestos if x.category_name == 'browOuterUpLeft'), 0.0)
-        ceja_R = next((x.score for x in gestos if x.category_name == 'browOuterUpRight'), 0.0)
-        val_cejas = max(ceja_L, ceja_R)
+        eyebrow_up_L = next((x.score for x in gestures if x.category_name == 'browOuterUpLeft'), 0.0)
+        eyebrow_up_R = next((x.score for x in gestures if x.category_name == 'browOuterUpRight'), 0.0)
+        val_eyebrows_up = max(eyebrow_up_L, eyebrow_up_R)
 
         # Sonrisa: (Da igual, es simétrica)
-        smile_L = next((x.score for x in gestos if x.category_name == 'mouthSmileLeft'), 0.0)
-        smile_R = next((x.score for x in gestos if x.category_name == 'mouthSmileRight'), 0.0)
-        val_sonrisa = (smile_L + smile_R) / 2
+        smile_L = next((x.score for x in gestures if x.category_name == 'mouthSmileLeft'), 0.0)
+        smile_R = next((x.score for x in gestures if x.category_name == 'mouthSmileRight'), 0.0)
+        val_smile = (smile_L + smile_R) / 2
         
         # Morritos y Boca: (Son gestos centrales, no afectan el espejo)
-        val_boca = next((x.score for x in gestos if x.category_name == 'jawOpen'), 0.0)
-        val_morritos = next((x.score for x in gestos if x.category_name == 'mouthPucker'), 0.0)
+        val_mouth_pucker = next((x.score for x in gestures if x.category_name == 'mouthPucker'), 0.0)
 
-          # Usamos una forma optimizada de buscar los valores
-        score_guiño = next((x.score for x in gestos if x.category_name == 'eyeBlinkRight'), 0.0)
-        score_boca = next((x.score for x in gestos if x.category_name == 'jawOpen'), 0.0)
+        # Usamos una forma optimizada de buscar los valores
+        score_right_twink = next((x.score for x in gestures if x.category_name == 'eyeBlinkRight'), 0.0)
+        score_open_mouth = next((x.score for x in gestures if x.category_name == 'jawOpen'), 0.0)
 
-        if RESULTADO_ACTUAL.face_landmarks:
-            nariz = RESULTADO_ACTUAL.face_landmarks[0][1] # Índice 1
-            nariz_x=nariz.x
-            nariz_y=nariz.y
+        if CURRENT_RESULT.face_landmarks:
+            nose = CURRENT_RESULT.face_landmarks[0][1] # Índice 1
+            nose_x=nose.x
+            nose_y=nose.y
             
         frame = cv2.flip(frame, 1)
 
 
-        # --- AQUI ESTARÍA EL CAMBIO SI USARAS GUIÑOS ---
-        # Si quisieras detectar guiño DERECHO real:
-        # guiño_derecho_real = next((x.score for x in gestos if x.category_name == 'eyeBlinkLeft'), 0.0)
-        # (Leemos el Left de la IA para obtener el Right Real)
-
         # --- 3. DIBUJAR TEXTO (Ahora saldrá bien) ---
         # Como estamos dibujando sobre el frame YA invertido, el texto sale derecho.
-
-        # GESTO 1: BOCA -> BTN A
-        if val_boca > UMBRAL_BOCA:
-            gamepad.press_button(button=buttons["boca"])
+        if score_open_mouth > input_structure["jawOpen"]["threshold"]:
+            gamepad.press_button(button=input_structure["jawOpen"]["input"])
             cv2.putText(frame, 'BTN A (BOCA)', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         else:
-            gamepad.release_button(button=buttons["boca"])
+            gamepad.release_button(button=input_structure["jawOpen"]["input"])
 
-        # GESTO 2: CEJAS -> BTN Y
-        if val_cejas > UMBRAL_CEJAS:
-            gamepad.press_button(button=buttons["cejas"])
+
+        if val_eyebrows_up > input_structure["eyeBrowsUp"]["threshold"]:
+            gamepad.press_button(button=input_structure["eyeBrowsUp"]["input"])
             cv2.putText(frame, 'BTN Y (CEJAS)', (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
         else:
-            gamepad.release_button(button=buttons["cejas"])
+            gamepad.release_button(button=input_structure["eyeBrowsUp"]["input"])
 
-        # GESTO 3: MORRITOS -> BTN X
-        if val_morritos > UMBRAL_MORRITOS:
-            gamepad.press_button(button=buttons["morritos"])
+        if val_mouth_pucker > input_structure["mouthPucker"]["threshold"]:
+            gamepad.press_button(button=input_structure["mouthPucker"]["input"])
             cv2.putText(frame, 'BTN X (MORRITOS)', (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
         else:
-            gamepad.release_button(button=buttons["morritos"])
+            gamepad.release_button(button=input_structure["mouthPucker"]["input"])
 
-        # GESTO 4: SONRISA -> BTN B
-        if val_sonrisa > UMBRAL_SONRISA:
-            gamepad.press_button(button=buttons["sonrisa"])
+
+        if val_smile > input_structure["smile"]["threshold"]:
+            gamepad.press_button(button=input_structure["smile"]["input"])
             cv2.putText(frame, 'BTN B (SONRISA)', (50, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
         else:
-            gamepad.release_button(button=buttons["sonrisa"])
+            gamepad.release_button(button=input_structure["smile"]["input"])
 
-            # 1. GUIÑO IZQUIERDO -> Botón A
-        if score_guiño > 0.5:
-            gamepad.press_button(button=buttons["guiño"])
+
+        if score_right_twink > input_structure["eyeBlinkRight"]["threshold"]:
+            gamepad.press_button(button=input_structure["eyeBlinkRight"]["input"])
             cv2.putText(frame, 'BTN A (GUIÑO)', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         else:
-            gamepad.release_button(button=buttons["guiño"])
+            gamepad.release_button(button=input_structure["eyeBlinkRight"]["input"])
 
         joystick_x = 0
         joystick_y = 0
-        if RESULTADO_ACTUAL.face_landmarks:
-            if nariz_x > 0.6:
+        if CURRENT_RESULT.face_landmarks:
+            if nose_x > 0.6:
                 joystick_x = -20000
                 cv2.putText(frame, '< IZQUIERDA', (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
-            elif nariz_x < 0.4:
+            elif nose_x < 0.4:
                 joystick_x = 20000
                 cv2.putText(frame, 'DERECHA >', (frame.shape[1]-200, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
-            if nariz_y > 0.6:
+            if nose_y > 0.6:
                 joystick_y = -20000
                 cv2.putText(frame, 'ABAJO', (frame.shape[1]//2 - 50, frame.shape[0]-50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
-            elif nariz_y < 0.4:
+            elif nose_y < 0.4:
                 joystick_y = 20000
                 cv2.putText(frame, 'ARRIBA', (frame.shape[1]//2 - 50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
             
